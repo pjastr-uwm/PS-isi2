b34ecf9453e24bf3744e765974d309c169c6c1d0
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-02-21 12:32:35 +0100

    zad8-10

---
+++ lab1/zad8-10/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("To jest cytat: \"Czesto uzywam jezyka C.\"\n");
+    printf("C:\\Program Files\\MojaAplikacja\\\n");
+    printf("C:\\\\Program Files\\\\MojaAplikacja\\\\\n");
+    return 0;
+}
