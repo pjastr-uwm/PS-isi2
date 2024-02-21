a76f0e4d2c67f0831ab7526bb5b58fa2571f2ec2
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-02-21 12:52:08 +0100

    lab1

--- lab1/zad8-10/main.c
+++ lab1/zad8-10/main.c
@@ -6,5 +6,6 @@ int main()
     printf("To jest cytat: \"Czesto uzywam jezyka C.\"\n");
     printf("C:\\Program Files\\MojaAplikacja\\\n");
     printf("C:\\\\Program Files\\\\MojaAplikacja\\\\\n");
+    printf("Specjalne znaki: \\t (tabulacja), \\n (nowa linia), %% (procent), \\ (ukoœnik wsteczny).");
     return 0;
 }
