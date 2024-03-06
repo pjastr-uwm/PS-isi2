0fced65f0059dd031ded8c95702563a57b188925
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-03-06 12:24:50 +0100

    10

---
+++ lab3/funkcje10/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int calculateFibonacciRecursively(int n){
+    if (n ==0)
+        return 0;
+    if (n==1)
+        return 1;
+    return calculateFibonacciRecursively(n-1)+calculateFibonacciRecursively(n-2);
+}
+
+int main()
+{
+    printf("%d\n",calculateFibonacciRecursively(0));
+    printf("%d\n",calculateFibonacciRecursively(1));
+    printf("%d\n",calculateFibonacciRecursively(2));
+    printf("%d\n",calculateFibonacciRecursively(3));
+    printf("%d\n",calculateFibonacciRecursively(4));
+    printf("%d\n",calculateFibonacciRecursively(5));
+    printf("%d\n",calculateFibonacciRecursively(6));
+    return 0;
+}
