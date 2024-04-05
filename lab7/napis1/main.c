3abdde9130e04ade8f81b44cfa3270dbe4430074
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-04-05 12:09:51 +0200

    napis1

--- lab7/napis1/main.c
+++ lab7/napis1/main.c
@@ -9,6 +9,56 @@ int length(char * txt){
     return counter;
 }
 
+int length2(char * txt){
+    int counter =0;
+    for(int i=0;txt[i] != 0;i++){
+        counter++;
+    }
+    return counter;
+}
+
+int length3(char * txt){
+    int counter =0;
+    int i=0;
+    while(txt[i] != 0){
+        counter++;
+        i++;
+    }
+    return counter;
+}
+
+int length4(char * txt){
+    int counter =0;
+    int i=0;
+    while(*(txt+i) != 0){
+        counter++;
+        i++;
+    }
+    return counter;
+}
+
+int length5(char * txt){
+    int counter =0;
+    int i=0;
+    while(*(txt+i)){
+        counter++;
+        i++;
+    }
+    return counter;
+}
+
+int length6(char*napis){
+    if (napis[0] == 0)
+        return 0;
+    return 1+length6(++napis);
+}
+
+int length7(char*napis){
+    if (*napis == 0)
+        return 0;
+    return 1+length7(++napis);
+}
+
 int main()
 {
     printf("%d\n", length("Olsztyn"));
@@ -18,5 +68,11 @@ int main()
     char * t2 = "ejhew7362";
     printf("%d\n", length(t1));
     printf("%d\n", length(t2));
+    printf("%d\n", length2("Olsztyn"));
+    printf("%d\n", length3("Olsztyn"));
+    printf("%d\n", length4("Olsztyn"));
+    printf("%d\n", length5("Olsztyn"));
+    printf("%d\n", length6("Olsztyn"));
+    printf("%d\n", length7("Olsztyn"));
     return 0;
 }
