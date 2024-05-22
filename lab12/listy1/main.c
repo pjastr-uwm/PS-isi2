5c732e537e652598dba8584cdb5a30987cdefef3
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-05-22 12:14:57 +0200

    listy1

---
+++ lab12/listy1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct element{
+    int x;
+    struct element * next;
+};
+
+int main()
+{
+    struct element * list = malloc(sizeof(struct element));
+    list->x = 4;
+    list->next = malloc(sizeof(struct element));
+    list->next->x = 5;
+    list->next->next = malloc(sizeof(struct element));
+    list->next->next->x  = -12;
+    list->next->next->next = NULL;
+    return 0;
+}
